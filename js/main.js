30fa01a080a4d04df64f2ce75c9f83396ee9d9aa
dexter <dexter@d3x.d3x.com>
2026-06-03 19:04:44 +0800

    fix capitalization

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('navbar-shrink');
-  } else {
-    navbar.classList.remove('navbar-shrink');
-  }
-});
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
@@ -14,5 +5,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     if (target) target.scrollIntoView({ behavior: 'smooth' });
   });
 });
-
-console.log('unifi site loaded');
